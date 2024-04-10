094862dfe4238059452831fb533316c32f596c3e
Ederson Ferreira dos Santos <edersonfs@gmail.com>
2024-04-10 16:02:32 -0300

    changes

--- Front/VallourecTelefonia.APP/src/app/components/responsaveis/responsavel-detalhe/responsavel-detalhe.component.ts
+++ Front/VallourecTelefonia.APP/src/app/components/responsaveis/responsavel-detalhe/responsavel-detalhe.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-responsavel-detalhe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './responsavel-detalhe.component.html',
-  styleUrl: './responsavel-detalhe.component.scss'
+  styleUrl: './responsavel-detalhe.component.scss',
 })
-export class ResponsavelDetalheComponent {
+export class ResponsavelDetalheComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
 
+  // get f(): any {
+  //   return this.form.controls;
+  // }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    this.form = this.formBuilder.group({
+      nome: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(100),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      ativo: [false],
+    });
+  }
+
+  resetForm(event: MouseEvent): void {
+    event.preventDefault();
+    this.form.reset();
+  }
 }
--- Front/VallourecTelefonia.APP/src/app/components/user/registration/registration.component.ts
+++ Front/VallourecTelefonia.APP/src/app/components/user/registration/registration.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ValidatorField } from '../../../helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    NgClass,
+    NgIf,
+  ],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
 })
-export class RegistrationComponent {}
+export class RegistrationComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+
+  constructor(public formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha'),
+    };
+
+    this.form = this.formBuilder.group(
+      {
+        nome: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(100),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmarSenha: ['', Validators.required],
+        ativo: false,
+      },
+      formOptions
+    );
+  }
+}
